14af34b1b65e2756e065c9afb65a179c3b78c3c0
tournevis <arthur.ro92@gmail.com>
2016-06-07 16:12:46 +0200

    fix emoji

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var Twit        = require('twit');
 var secret      = require("./secret.json");
-
+  //
 var T = new Twit({
   consumer_key:         secret.twitCred[0]
 , consumer_secret:      secret.twitCred[1]
 , access_token:         secret.twitCred[2]
 , access_token_secret:  secret.twitCred[3]
 });
-
+var
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
 
 app.listen(port);
@@ -27,12 +27,33 @@ app.get('/', function(req,res){
 // API ROUTES -------------------
 // we'll get to these in a second
 var apiRoutes = express.Router();
+var emojiArray = ["🌲", "🌳","🌴","🌺","🍉","🍐","🍏","😃","👍","🌟"];
+
+var replaceEmoji = function(sentenceTw){
+    var mapObj = {
+     A:emojiArray[0],
+     Z:emojiArray[1],
+     E:emojiArray[2],
+     R:emojiArray[3],
+     T:emojiArray[4],
+     Y:emojiArray[5],
+     U:emojiArray[6],
+     I:emojiArray[7],
+     O:emojiArray[8]
+    };
+  var re = new RegExp(Object.keys(mapObj).join("|"),"gi");
+  sentenceTw = sentenceTw.replace(re, function(matched){
+    return mapObj[matched];
+  });
+  return sentenceTw;
+}
 
 apiRoutes.post('/tweet', function(req,res){
-    var myTweet = req.body.tweet ;
-    var myToken = req.body.token ;
+  console.log("Got a request");
+    var myTweet = replaceEmoji(req.body.tweet);
+    var myToken = req.body.token;
     if( myTweet != "" && myToken == secret.token){
-      T.post('statuses/update', { status: "🐦" + myTweet }, function(err, data, response) {
+      T.post('statuses/update', { status: myTweet }, function(err, data, response) {
         console.log(data);
       });
       return res.status(200).send({
