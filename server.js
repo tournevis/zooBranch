77872d34a669ecb6ac81e2b084371e09d743aec8
tournevis <arthur.ro92@gmail.com>
2016-06-08 10:20:53 +0200

    hello

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var T = new Twit({
 , access_token:         secret.twitCred[2]
 , access_token_secret:  secret.twitCred[3]
 });
-var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+var port = process.env.PORT || 8000; // used to create, sign, and verify tokens
 
 app.listen(port);
 console.log('Magic happens at http://localhost:' + port);
